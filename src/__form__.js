ecd75848ce5e621383148f6f2f745f55d6475609
AkhiSharmin <akhisharmin100@gmail.com>
2025-03-14 22:23:34 +0600

    update

---
+++ src/__form__.js
@@ -0,0 +1,3 @@
+/***
+ * 1. use form tag and onSubmit handler with event(e) form e access e.target.[name of the input field].value
+ */
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,10 +1,22 @@
 const SimpleForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+    console.log(e.target.phone.value);
+    console.log("form submitted");
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
         <br />
-        <button>Submit</button>
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="text" name="phone" id="" />
+        <br />
+        <input type="submit" value="submit" />
       </form>
     </div>
   );
