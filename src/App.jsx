caa9c433c47e6758df56165fd105c27d19dc7050
AkhiSharmin <akhisharmin100@gmail.com>
2025-03-18 22:46:07 +0600

    added reuseable form

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
 import "./App.css";
-import ReuseableForm from "./components/ReuseableForm/ReuseableForm";
+import Grandpa from "./components/Grandpa/Grandpa";
+// import ReuseableForm from "./components/ReuseableForm/ReuseableForm";
 // import HookForm from "./components/HookForm/HookForm";
 // import RefForm from "./components/RefForm/RefForm";
 // import StateFullForm from "./components/StateFullForm/StateFullForm";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
-  const handleSingUpSubmit = (e) => {
-    e.preventDefault();
-  };
+  // const handleSingUpSubmit = (data) => {
+  //   console.log("sing up data", data);
+  // };
+
+  // const handleUpdateProfile = (data) => {
+  //   console.log(" update profile data", data);
+  // };
 
   return (
     <>
-      <h1>Simple Form</h1>
+      <h1>Form Form</h1>
+      <Grandpa></Grandpa>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullForm></StateFullForm> */}
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
-      <ReuseableForm
-        formTitle={"Sing Up"}
-        handleSubmit={handleSingUpSubmit}
-      ></ReuseableForm>
+      {/* <ReuseableForm formTitle={"Sing Up"} handleSubmit={handleSingUpSubmit}>
+        <div>
+          <h2>Sing Up</h2>
+          <p>Please sing up right now</p>
+        </div>
+      </ReuseableForm>
       <ReuseableForm
         formTitle={"Profile Update"}
+        handleSubmit={handleUpdateProfile}
         submitBtnText={"Update"}
-      ></ReuseableForm>
+      >
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always Keep your profile update</p>
+        </div>
+      </ReuseableForm> */}
     </>
   );
 }
--- src/components/ReuseableForm/ReuseableForm.jsx
+++ src/components/ReuseableForm/ReuseableForm.jsx
@@ -1,17 +1,28 @@
 const ReuseableForm = ({
-  formTitle,
+  //   formTitle,
   handleSubmit,
+  children,
   submitBtnText = "Submit",
 }) => {
+  const handleLocalSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    handleSubmit(data);
+  };
+
   return (
     <div>
-      <h2>{formTitle}</h2>
-      <form onSubmit={handleSubmit}>
+      {children}
+      <form onSubmit={handleLocalSubmit}>
         <input type="text" name="name" />
         <br />
         <input type="email" name="email" id="" />
         <br />
-        <input type="text" name="phone" id="" />
+        <input type="password" name="password" id="" />
         <br />
         <input type="submit" value={submitBtnText} />
       </form>
