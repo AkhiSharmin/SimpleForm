4980152015eb86ac1376e4c4485cf5bf1a59ef8d
AkhiSharmin <akhisharmin100@gmail.com>
2025-03-18 16:44:44 +0600

    handle sing up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import "./App.css";
-import HookForm from "./components/HookForm/HookForm";
+import ReuseableForm from "./components/ReuseableForm/ReuseableForm";
+// import HookForm from "./components/HookForm/HookForm";
 // import RefForm from "./components/RefForm/RefForm";
 // import StateFullForm from "./components/StateFullForm/StateFullForm";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
+  const handleSingUpSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1>Simple Form</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullForm></StateFullForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReuseableForm
+        formTitle={"Sing Up"}
+        handleSubmit={handleSingUpSubmit}
+      ></ReuseableForm>
+      <ReuseableForm
+        formTitle={"Profile Update"}
+        submitBtnText={"Update"}
+      ></ReuseableForm>
     </>
   );
 }
--- src/components/ReuseableForm/ReuseableForm.jsx
+++ src/components/ReuseableForm/ReuseableForm.jsx
@@ -1,7 +1,20 @@
-const ReuseableForm = () => {
+const ReuseableForm = ({
+  formTitle,
+  handleSubmit,
+  submitBtnText = "Submit",
+}) => {
   return (
     <div>
-      <h2>ReuseableForm</h2>
+      <h2>{formTitle}</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="text" name="phone" id="" />
+        <br />
+        <input type="submit" value={submitBtnText} />
+      </form>
     </div>
   );
 };
