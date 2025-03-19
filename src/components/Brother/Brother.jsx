b3c8a8b1d229cde9731af7656f5104dcc572ee23
AkhiSharmin <akhisharmin100@gmail.com>
2025-03-20 00:18:25 +0600

    using contextApi

--- src/components/Brother/Brother.jsx
+++ src/components/Brother/Brother.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { AssetContext, MoneyContext } from "../Grandpa/Grandpa";
+
 const Brother = () => {
+  const [money] = useContext(MoneyContext);
+  const gold = useContext(AssetContext);
   return (
     <div>
       <h2>Brother</h2>
+      <p>Grandpa Money: {money}</p>
+      <p>Has: {gold}</p>
     </div>
   );
 };
--- src/components/Cousin/Cousin.jsx
+++ src/components/Cousin/Cousin.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import Friend from "../Friend/Friend";
 import Special from "../Special/Special";
+import { AssetContext } from "../Grandpa/Grandpa";
 
 const Cousin = ({ name, asset }) => {
+  const gold = useContext(AssetContext);
   return (
     <div>
       <h2>Cousin</h2>
       <p>{name}</p>
-      <section>{asset && <Special asset={asset}></Special>}</section>
+      <section>
+        {asset && <Special asset={asset}></Special>}
+        {name === "Siam" && <Friend></Friend>}
+        {name === "Sabiha" && <p>Has: {gold}</p>}
+      </section>
     </div>
   );
 };
